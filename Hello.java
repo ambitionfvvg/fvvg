092ae79354cbc011f3556c8cf448d9d16336031a
fvvg <709610863@qq.com>
2016-11-30 09:02:26 +0800

    chong xin ti jiao

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class  Hello
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World11111112222222222!");
+		System.out.println("Hello World11111112222222222000000000!");
 		
 
 	}
