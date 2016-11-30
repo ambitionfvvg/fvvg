4ca49253941ad01a8c03f57c8588feb74fa8b3a5
fvvg <709610863@qq.com>
2016-11-30 08:41:20 +0800

    updata Hello.java add two line

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ class  Hello
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello World!你好吗");
+		System.out.println("Hello World!你不好吗");
+
 	}
 }
