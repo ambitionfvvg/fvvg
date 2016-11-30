9b60df20082e9c575e2642f546da86b8eefa67d6
fvvg <709610863@qq.com>
2016-11-30 11:39:05 +0800

    xiu gai wenjian

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ class  Hello
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World11111112222222222000000000!");
+		System.out.println("Hello World11111112222222222000000000!");
+		System.out.println("Hello World11111112222222222000000000!");
 		
 
 	}
