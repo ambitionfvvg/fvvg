cb9497aa47d51c6a643cffb657c7369301aac347
fvvg <709610863@qq.com>
2016-11-30 08:46:44 +0800

    updata Hello.java and add Demo.java

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ class  Hello
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!你好吗");
-		System.out.println("Hello World!你不好吗");
+		System.out.println("Hello World1111111!");
+		
 
 	}
 }
